e4ce6cf34b77d438063822338ab929cd279f3b21
AnnyaB <anabordin2008@gmail.com>
2024-04-21 14:28:46 -0300

    verificacao salario positivo

--- SalarioPorcentagem/Program.cs
+++ SalarioPorcentagem/Program.cs
@@ -2,15 +2,22 @@
 
 int res = 0;
 
+Console.WriteLine("-Valor de acrescimo do salário-");
 while (res == 0)
 {
     double salarioInicial = 0, salarioFinal = 0, acres = 0;
 
-    Console.WriteLine("Digite o salário inicial, com virgulas para indicar centavos:");
-    salarioInicial = double.Parse(Console.ReadLine());
+    while (salarioInicial <= 0)
+    {
+        Console.WriteLine("Digite o salário inicial, com virgulas para indicar centavos:");
+        salarioInicial = double.Parse(Console.ReadLine());
+    }
 
-    Console.WriteLine("Digite o salário final, com virgulas para indicar centavos:");
-    salarioFinal = double.Parse(Console.ReadLine());
+    while (salarioFinal <= salarioInicial)
+    {
+        Console.WriteLine("Digite o salário com acrescimo, com virgulas para indicar centavos:");
+        salarioFinal = double.Parse(Console.ReadLine());
+    }
 
     acres = ((salarioFinal * 100) / salarioInicial);
     acres = acres - 100;
